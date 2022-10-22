6900ffe69befca0b14c7c1953cbafc7cb45a0862
MatthewOglesby <matt.oglesby99@gmail.com>
2022-10-22 10:39:24 -0500

    Navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,21 @@ const Navbar = ({ logout, token }) => {
     return (
         <header>
             <nav className='navLinks'>
-                <h3 className='navLinks3'>FitnessTrackr</h3>
-                <Link to='/home' className='navLinks2'>Home</Link>
-                <Link to='/routines' className='navLinks2'>Routines</Link>
-                <Link to='/activities' className='navLinks2'>Activities</Link>
+                <h3 className='navLinks3'>FitnessTracker</h3>
 
                 {
                     token ? (
                         <>
+                            <Link to='/home' className='navLinks2'>Home</Link>
+                            <Link to='/routines' className='navLinks2'>Routines</Link>
                             <Link to='/MyRoutines' className='navLinks2'>My Routines</Link>
+                            <Link to='/activities' className='navLinks2'>Activities</Link>
                             <Link to='/home' onClick={() => { logout(); }} className='navLinks2'>Logout</Link>
                         </>
                     ) : (
                         <>
+                            <Link to='/routines' className='navLinks2'>Routines</Link>
+                            <Link to='/activities' className='navLinks2'>Activities</Link>
                             <Link to='/register' className='navLinks2'>Register</Link>
                             <Link to='/login' className='navLinks2'>Login</Link>
                         </>
